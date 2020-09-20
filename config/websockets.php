5eb08233a032d669d0d5caaed837379ad3ac6112
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-09-20 11:17:35 -0500

    agregando nuestros proyectos a la config de websockets

--- config/websockets.php
+++ config/websockets.php
@@ -24,10 +24,9 @@ return [
     'apps' => [
         [
             'id' => env('PUSHER_APP_ID'),
-            'name' => env('APP_NAME'),
+            'name' => 'realtime-apps',
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
-            'path' => env('PUSHER_APP_PATH'),
             'capacity' => null,
             'enable_client_messages' => false,
             'enable_statistics' => true,
